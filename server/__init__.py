f97dd4fa30e9e100c10963207601bbf501cd91e4
Scott Adie <scottynosabe@gmail.com>
2019-11-09 14:39:14 -0500

    Added support for configuration

--- server/__init__.py
+++ server/__init__.py
@@ -6,14 +6,19 @@ from flask import Flask
 def create_app(test_config: dict = None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, 'upscoot.sqlite')
+        DATABASE=os.path.join(app.instance_path, 'upscoot.sqlite')  # This will move to config file eventually.
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_object('config')  # Load default settings
+        app.config.from_pyfile('config.py', silent=True)  # load application specific settings
     else:
         app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     try:
         os.makedirs("uploads")
